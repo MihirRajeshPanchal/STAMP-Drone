d8420e8b75f470f87aca9fa3fa82a00cb93f42a2
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2023-03-13 12:46:08 +0530

    Added DroneGPT

--- Face Recognition/01_face_dataset.py
+++ Face Recognition/01_face_dataset.py
@@ -12,7 +12,7 @@ face_id = input('\n enter user id end press <return> ==>  ')
 
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 # Initialize individual sampling face count
-count = -30
+count = 0
 
 while(True):
 
---
+++ Object Detection/yoloVideo.py
@@ -0,0 +1,128 @@
+import cv2
+import numpy as np
+import time
+import glob
+import os
+global cnt
+cnt=0
+
+def load_yolo():
+
+    net=cv2.dnn.readNetFromDarknet("python\yolov3_testing.cfg","python\yolov3.weights")
+
+    with open("python\coco.names", "r") as f:
+      classes = [line.strip() for line in f.readlines()]
+
+    output_layers = [layer_name for layer_name in net.getUnconnectedOutLayersNames()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    return net, classes, colors, output_layers
+
+
+def start_video():
+   model, classes, colors, output_layers = load_yolo()
+   cap = cv2.VideoCapture(0)
+   cnte=0
+   ret=True
+   # out = cv2.VideoWriter('videos/proc.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 20.0, (416,416))
+   while ret:
+      ret, frame = cap.read()
+      #print(ret,cnte)
+      if not ret:
+         break
+      # cv2.imshow('Win',frame)
+      # cv2.waitKey(1500)
+      try:
+         height, width, channels = frame.shape
+         blob, outputs = detect_objects(frame, model, output_layers)
+      except:
+         pass
+
+      boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+      draw_labels(boxes, confs, colors, class_ids, classes, frame)
+      cnte+=1
+      # out.write(img) #--- out----FUNC
+      key = cv2.waitKey(1)
+      if key == 27:
+         break
+   cap.release()
+
+
+
+
+def get_box_dimensions(outputs, height, width):
+   boxes = []
+   confs = []
+   class_ids = []
+   prev_frame_time=0
+   new_prev_frame_time=0
+
+
+   for output in outputs:
+      for detect in output:
+         scores = detect[5:]
+         class_id = np.argmax(scores)
+         conf = scores[class_id]
+         if conf > 0.4:   #Try .6
+            center_x = int(detect[0] * width)
+            center_y = int(detect[1] * height)
+            w = int(detect[2] * width)
+            h = int(detect[3] * height)
+            x = int(center_x - w/2)
+            y = int(center_y - h / 2)
+            boxes.append([x, y, w, h])
+            confs.append(float(conf))
+            class_ids.append(class_id)
+   # codec = cv2.VideoWriter_fourcc(*"MJPG")
+   # new_frame_time = time.time()
+   # fps = 1 / (new_frame_time - prev_frame_time)
+   # prev_frame_time = new_frame_time
+   # fps_n = int(fps)
+
+   return boxes, confs, class_ids
+
+def detect_objects(img, net, outputLayers):
+    blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(outputLayers)
+    return blob, outputs
+
+def draw_labels(boxes, confs, colors, class_ids, classes, img):
+   global cnt
+   indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4) #(0.2,0.2) // (0.7,0.6)
+   font = cv2.FONT_HERSHEY_DUPLEX
+   # image_folder = 'data-set-race-01'
+   video_file = './public/python/videos/proc.mp4'
+   image_size = (416, 416)
+   fps = 24
+   # out = cv2.VideoWriter(video_file, cv2.VideoWriter_fourcc('M','P','E','G'), fps, image_size)
+
+   for i in range(len(boxes)):
+      if i in indexes:
+         x, y, w, h = boxes[i]
+         label = str(classes[class_ids[i]])
+         # color = colors[i]
+
+         if confs[i] > 0.85:
+            color=(0,255,0)#GREEN
+         elif confs[i] > 0.6:
+            color = (255,0,0)#ORange
+         elif confs[i] >0.4:
+            color=(255,0,255)#RED
+
+
+         cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+         cv2.putText(img, label + str(round(confs[i]*100,2)), (x, y - 5), font, 1, color, 1)
+         # out.write(img)
+
+   cv2.imshow("Image", img)
+   cv2.imwrite('./public/python/frames/'+str(cnt)+'.jpg',img)
+   #print('\t',cnt)
+   cnt += 1
+
+
+#----------------------------------MAIN -------------------
+# if name == 'main':
+def yolo():
+   start_video()
+      
+yolo()
