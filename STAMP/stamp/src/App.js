1ef4fd1a9d7343132d420c5241d067175d4db3c8
prinkaldoshi27 <prinkaldoshi@gmail.com>
2023-03-30 13:13:28 +0530

    website started P

--- STAMP/stamp/src/App.js
+++ STAMP/stamp/src/App.js
@@ -11,30 +11,13 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Navbar from './components/Navbar';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Homepage></Homepage> 
     </ChakraProvider>
   );
 }
