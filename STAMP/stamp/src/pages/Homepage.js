9425f448247efe8a0520053571f7ef33a08e773d
prinkaldoshi27 <prinkaldoshi@gmail.com>
2023-03-30 14:51:18 +0530

    added files P

---
+++ STAMP/stamp/src/pages/Homepage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Image,
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../ColorModeSwitcher';
+import { Logo } from '../Logo';
+import Navbar from '../components/Navbar';
+import CTA from '../components/CTA';
+import bg1 from '../assets/bg1.png';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Navbar></Navbar>
+      <Image src={bg1} alt='Background 1' height={600} width={1300}/>
+      <CTA></CTA>
+    </ChakraProvider>
+  );
+}
+
+export default App;
